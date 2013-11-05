6961cb8b4f86eaa3432d4be9ceaeb5070ce0f26e
jitendra-budwal <budwal.j@gmail.com>
2013-11-04 22:15:23 -0600

    Login tests

--- src/TestTempConv.java
+++ src/TestTempConv.java
@@ -1,31 +1,108 @@
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import com.google.common.base.Function;
 
 
 public class TestTempConv {
 	
    static long t0;
    
-   public void test_login_success(WebDriver webdriver, String username,
+   public static void main(String[] args) throws Exception {
+       String success = "Online temperature conversion calculator";
+       String failure = "Bad Login";
+       String lockout = "Frequent Login";
+     
+       // The Firefox driver supports javascript
+       //WebDriver driver = new FirefoxDriver();
+       WebDriver driver = new FirefoxDriver();
+
+       TestTempConv gs = new TestTempConv();
+
+       t0 = System.currentTimeMillis();
+
+       driver.get("http://adnan.appspot.com/testing-lab-login.html");
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ": Attempt login 3 times with bad password.");
+       gs.testLogin(driver, "charley", "apple", failure);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  1st attempt complete.");
+
+       // login attempts in quick succession will result in lockout delay screen.
+       //multiple ways to get back to login page use url here
+       driver.get("http://adnan.appspot.com/testing-lab-login.html");
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  2nd attempt initiated.");
+       gs.testLogin(driver, "charley", "banana", lockout);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  2nd attempt complete.");
+
+       // login attempts in quick succession will result in lockout delay screen.
+       //multiple ways to get back to login page use back navigation
+       driver.navigate().back();
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  3rd attempt initiated.");
+       gs.testLogin(driver, "charley", "carrot", lockout);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  3rd attempt complete.");
+
+       driver.get("http://adnan.appspot.com/testing-lab-login.html");
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ": 4th attempt initiated with good password expect lockout.");    
+       gs.testLogin(driver, "charley", "china", lockout);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  4th attempt complete.");
+
+       driver.get("http://adnan.appspot.com/testing-lab-login.html");
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  bad password attempt");
+       gs.testLogin(driver, "bob", "banana", failure);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+
+       driver.findElement(By.tagName("a")).click();
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  bad password attempt");
+       gs.testLogin(driver, "andy", "banana", failure);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       
+       driver.findElement(By.tagName("a")).click();
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  attempt to login BOB ");
+       gs.testLogin(driver, " BOB ", "bathtub", lockout);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       driver.navigate().back();
+
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  attempt to login andy ");
+       gs.testLogin(driver, "andy", "apple", success);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       driver.navigate().back();
+
+       
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  attempt to login charley ");
+       gs.testLogin(driver, "charley", "china", success);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       driver.navigate().back();
+
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  attempt to login ANDY ");
+       gs.testLogin(driver, "ANDY", "apple", lockout);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       driver.navigate().back();
+
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  attempt to login bob ");
+       gs.testLogin(driver, "bob", "bathtub", success);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+       driver.navigate().back();
+
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  attempt to login ChArLeY ");
+       gs.testLogin(driver, "ChArLeY", "china", success);
+       if (driver.getTitle().compareTo(lockout) == 0) testLockout(driver);
+
+       //end of tests.
+       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  end of Tests.");
+       driver.close();
+
+    }
+
+   public void testLogin(WebDriver webdriver, String username,
 		   String password, String expect) {
-     //go to login page
-//     webdriver.get("http://adnan.appspot.com/testing-lab-login.html");
-//     assertEquals("Online temperature conversion calculator", webdriver.getTitle());
+     //expect to start at login page
+     assertEquals("Dummy login page for Testing lab", webdriver.getTitle());
 
      WebElement userE = webdriver.findElement(By.name("userId"));
      userE.clear();
@@ -41,7 +118,6 @@ public class TestTempConv {
     	 System.out.println(((System.currentTimeMillis() - t0)/1000) + ": Login Test Failed: " + username); 	 
     	 System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  Expected: [" + expect + "] Actual: [" + webdriver.getTitle() + "]"); 	 
      };
-     
    }
    
    public void test_temp_conversion(WebDriver driver, String fahren, String expected) {
@@ -59,116 +135,30 @@ public class TestTempConv {
 	   assertEquals(expected, driver.findElement(By.tagName("h2")).getText());
    }
 	      
-   public static void main(String[] args) throws Exception {
-       // The Firefox driver supports javascript
-       //WebDriver driver = new FirefoxDriver();
-       WebDriver driver = new FirefoxDriver();
-
-       String success = "Online temperature conversion calculator";
-       String failure = "Bad Login";
-       String slowdown = "Frequent Login";
- 
-       driver.get("http://adnan.appspot.com/testing-lab-login.html");
+   private static void testLockout(WebDriver driver) throws InterruptedException {
+	   Long t1;
+	   Boolean exceededTimeout;
+	   // check length of lockout
+       t1 = System.currentTimeMillis() + 11000;
+       exceededTimeout = true;
        
-       TestTempConv gs = new TestTempConv();
-
-       t0 = System.currentTimeMillis();
-
-       System.out.println(((System.currentTimeMillis() - t0)/1000) + ": Attempt login 3 times with bad password.");
-       gs.test_login_success(driver, "charley", "apple", failure);
-       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  1st attempt complete.");
-
-//     System.out.println(" Passed charley login test.");
-//     driver.navigate().back();
-//     driver.findElement(By.tagName("a")).click();
-       driver.get("http://adnan.appspot.com/testing-lab-login.html");
-
-       gs.test_login_success(driver, "charley", "china", slowdown);
-       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  2nd attempt complete.");
-//   System.out.println(" Passed charley login test.");
-//   driver.navigate().back();
-//   driver.findElement(By.tagName("a")).click();
-    // Waiting 20 seconds for an element to be present on the page, checking
-       // for its presence once every 1 seconds.
-       Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-           .withTimeout(20, TimeUnit.SECONDS)
-           .pollingEvery(1, TimeUnit.SECONDS)
-           .ignoring(NoSuchElementException.class);
-
-       WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
-         public WebElement apply(WebDriver driver) {
-           return driver.findElement(By.tagName("a"));
-         }
-       });
-//       long t1 = System.currentTimeMillis() + 20000;
-//       while System.currentTimeMillis() < t1)
-//       (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
-//           public Boolean apply(WebDriver d) {
-//    		   d.navigate().refresh();
-//        	   if (d.findElement(By.tagName("a")) == null) {
-//        	       System.out.print( ".");
-//        		   return false;
-//        	   } else {
-//        	       System.out.println( "#");
-//        		   return true;
-//        	   }
-//           }
-//       });
-       
-       driver.get("http://adnan.appspot.com/testing-lab-login.html");
-
-       gs.test_login_success(driver, "charley", "carrot", slowdown);
-       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  3rd attempt complete.");
-//       System.out.println(" Passed charley login test.");
-//       driver.navigate().back();
-//       driver.findElement(By.tagName("a")).click();
-       driver.get("http://adnan.appspot.com/testing-lab-login.html");
-
-       System.out.println(((System.currentTimeMillis() - t0)/1000) + ": Attempt login 4 with good password expect lockout.");    
-       gs.test_login_success(driver, "charley", "china", success);
-       System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  expect lock out.");
-//       System.out.println(" Passed charley login test.");
-//       driver.navigate().back();
-//       driver.findElement(By.tagName("a")).click();
-       driver.get("http://adnan.appspot.com/testing-lab-login.html");
-
-       gs.test_login_success(driver, "bob", "banana", failure);
-//       System.out.println(" Passed bob login test.");
-//       driver.navigate().back();
-       driver.findElement(By.tagName("a")).click();
-
-       gs.test_login_success(driver, "andy", "banana", failure);
-//       System.out.println(" Passed andy login test.");
-//       driver.navigate().back();
-       driver.findElement(By.tagName("a")).click();
-
-       gs.test_login_success(driver, " BOB ", "bathtub", success);
-//     System.out.println(" Passed BOB login test.");
-     driver.navigate().back();
-
-       gs.test_login_success(driver, "andy", "apple", success);
-//       System.out.println(" Passed andy login test.");
-       driver.navigate().back();
-
-       gs.test_login_success(driver, "bob", "banana", failure);
-//       System.out.println(" Passed bob login test.");
-//       driver.navigate().back();
-       driver.findElement(By.tagName("a")).click();
-
-       gs.test_login_success(driver, "charley", "china", success);
-//       System.out.println(" Passed charley login test.");
-       driver.navigate().back();
-
-       gs.test_login_success(driver, "ANDY", "apple", success);
-//       System.out.println(" Passed ANDY login test.");
-       driver.navigate().back();
-
-       gs.test_login_success(driver, "bob", "bathtub", success);
-//       System.out.println(" Passed bob login test.");
-       driver.navigate().back();
-
-       gs.test_login_success(driver, "ChArLeY", "china", success);
-//       System.out.println(" Passed ChArLeY login test.");
-
+       do {
+    	   if (driver.getTitle().compareTo("Frequent Login") == 0) {
+    		   Thread.sleep(1000);
+//    		   System.out.println(((System.currentTimeMillis() - t0)/1000) + ":  Title = " + driver.getTitle());
+    		   System.out.print(".");
+    		   driver.navigate().refresh();
+    		   //Now the alert appears. 
+    		   Alert alert = driver.switchTo().alert();
+    		   alert.accept();
+    	   } else {
+    		   exceededTimeout = false;
+    		   break;
+    	   }
+       } while (System.currentTimeMillis() < t1);
+       System.out.println("|");
+       if (exceededTimeout) {
+       		System.out.println(((System.currentTimeMillis() - t0)/1000) + ":   Exceeded lockout time.");
+       }
     }
 }
